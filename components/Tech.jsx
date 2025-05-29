e7ae1ec1626747b8913193a1c8d7f3a8b485c9e2
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-05-30 01:29:39 +0530

    Add files

--- components/Tech.jsx
+++ components/Tech.jsx
@@ -1,19 +1,55 @@
 import React from "react";
-import TechStack from "./TechStack";
 
+function Tech() {
+  return (
+    <>
+      <div className="flex justify-center mt-10">
+        <div className="font-black text-xl">What I Work With</div>
+      </div>
 
-function Tech(){
-    return(
-        <>
-        <div className="flex justify-center mt-10">
-            <div className="font-black relative right-14">What I Work With</div>
+      <div className="flex justify-center mt-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 max-w-4xl">
+          {/* Each Tech Item */}
+          <div className="flex flex-col items-center">
+            <img src="Images/HTML_Logo.png" alt="HTML" className="w-12 h-12" />
+            <p className="mt-1">HTML</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src="Images/Css_Logo.jpg" alt="CSS" className="w-12 h-12" />
+            <p className="mt-1">CSS</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src="Images/js.jpg" alt="JavaScript" className="w-12 h-12" />
+            <p className="mt-1">JS</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src="Images/react.png" alt="React" className="w-12 h-12" />
+            <p className="mt-1">React</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src="Images/tailwind.png" alt="Tailwind" className="w-18 h-12" />
+            <p className="mt-1">Tailwind</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src="Images/git.png" alt="Git" className="w-12 h-12" />
+            <p className="mt-1">Git</p>
+          </div>
+
+          {/* More Tech Items */}
+          <div className="flex flex-col items-center">
+            <img src="Images/github.png" alt="Node.js" className="w-12 h-12 rounded-4xl" />
+            <p className="mt-1">Github</p>
+            
+          </div>
         </div>
-        <div className="flex gap-2">
-            <TechStack imgurl="Images/HTML_Logo.jpg" tech="HTML" />
-
-        </div>
-        
-        </>
-    )
+      </div>
+    </>
+  );
 }
-export default Tech
+
+export default Tech;
--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -3,10 +3,6 @@ import React from "react";
 function TechStack({imgurl,tech}){
     return(
         <>
-        <div className="flex justify-center items-center gap-2">
-            <img className="h-10 w-10" src={imageurl} alt="" />
-            <div>{tech}</div>
-        </div>
         </>
     )
 }
